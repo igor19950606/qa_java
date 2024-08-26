f7730748bacbd4da08e731f43d802a30ca01e68d
Igor Ponomarev <Alfrankov19950606@yandex.com>
2024-08-26 08:27:11 +0300

    Исправление замечаний

--- src/test/java/LionTest.java
+++ src/test/java/LionTest.java
@@ -1,56 +1,57 @@
 import com.example.Feline;
 import com.example.Lion;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 public class LionTest {
+
     @Mock
     private Feline mockFeline;
 
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
     }
 
     @Test
-    void testGetKittens() throws Exception {
+    public void testGetKittens() throws Exception {
         when(mockFeline.getKittens()).thenReturn(3);
-        Lion lion = new Lion("Самец");
+        Lion lion = new Lion("Самец", mockFeline);
         int kittens = lion.getKittens();
-        assertEquals(1, kittens);
+        assertEquals(3, kittens);
     }
 
     @Test
-    void testDoesHaveMane() throws Exception {
-        Lion lionMale = new Lion("Самец");
-        Lion lionFemale = new Lion("Самка");
+    public void testDoesHaveMane() throws Exception {
+        Lion lionMale = new Lion("Самец", mockFeline);
+        Lion lionFemale = new Lion("Самка", mockFeline);
         assertTrue(lionMale.doesHaveMane());
         assertFalse(lionFemale.doesHaveMane());
     }
 
     @Test
-    void testGetFood() throws Exception {
+    public void testGetFood() throws Exception {
         List<String> expectedFood = Arrays.asList("Животные", "Птицы", "Рыба");
         when(mockFeline.getFood("Хищник")).thenReturn(expectedFood);
-        Lion lion = new Lion("Самец");
+        Lion lion = new Lion("Самец", mockFeline);
         List<String> food = lion.getFood();
         assertNotNull(food);
         assertEquals(expectedFood, food);
     }
 
     @Test
-    void testInvalid() {
+    public void testInvalidSex() {
         String invalidSex = "Исключение";
         Exception exception = assertThrows(Exception.class, () -> {
-            new Lion(invalidSex);
+            new Lion(invalidSex, mockFeline);
         });
         assertEquals("Используйте допустимые значения пола животного - самей или самка", exception.getMessage());
     }
